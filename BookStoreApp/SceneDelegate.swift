122f7bc6e4ef816a789e05b0a359aa90a995acaf
RetroJet <rretrojet@gmail.com>
2025-08-25 15:51:54 +0200

    Optimized the project

--- BookStoreApp/SceneDelegate.swift
+++ BookStoreApp/SceneDelegate.swift
@@ -8,20 +8,44 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    private let allTabBarItems = TabBarItem.allTabBarItems
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let bookRepository = BookRepository()
+        let tabBarController = TabBarController()
         
-        let tabBarController = TabBarController(bookRepository: bookRepository)
+        tabBarController.viewControllers?.enumerated().forEach { index, vc in
+            guard let navVC = vc as? UINavigationController else { return }
+            pushViewController(index: index, controller: navVC)
+        }
         
         window?.rootViewController = UINavigationController(rootViewController: tabBarController)
         window?.makeKeyAndVisible()
+    }
+}
+
+extension SceneDelegate {
+    func pushViewController(index: Int, controller: UINavigationController) {
+        let bookManager: IBookManager = BookManager()
+        
+        if bookManager.isEmpty() {
+            let bookRepository = BookRepository()
+            let books = bookRepository.getBookTypes()
+            bookManager.addBooks(books)
+        }
+        
+        switch allTabBarItems[index] {
+        case .home:
+            let collectionVC = CollectionViewController(manager: bookManager)
+            controller.pushViewController(collectionVC, animated: false)
+        case .search:
+            controller.pushViewController(MultipleSectionsViewController(), animated: false)
+        }
     }
 }
 
